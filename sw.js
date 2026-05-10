698a852196a3a388bb983057d97850f60fabe39e
sergutelo <sergutel@gmail.com>
2026-05-10 15:07:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'quecomemos-v30';
-const urlsToCache = [
+const CACHE_NAME = 'quecomemos-v31';
+const ASSETS = [
   './',
   './index.html',
   './style.css',
@@ -7,54 +7,37 @@ const urlsToCache = [
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
-  './qr-girl.png',
-  'https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;700;900&display=swap'
+  './qr-girl.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => {
-      // Toma el control de todos los clientes inmediatamente
-      return self.clients.claim();
-    })
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
+    fetch(event.request)
       .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+        return response;
       })
+      .catch(() => caches.match(event.request))
   );
 });
 
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
-  }
-  // Responde con la versión actual del SW para detectar actualizaciones silenciosas
-  if (event.data && event.data.type === 'GET_VERSION') {
-    event.source.postMessage({ type: 'SW_VERSION', version: CACHE_NAME });
   }
 });
